2aa10e1297a77462e0139bea953634decc680dfa
Jesper Fogh <foghjesperjhf@gmail.com>
2020-05-29 16:00:53 +0200

    Added Google Analytics.

--- nuxt.config.js
+++ nuxt.config.js
@@ -27,7 +27,9 @@ module.exports = {
   /*
   ** Plugins to load before mounting the App
   */
-  plugins: [],
+  plugins: [
+    { src: '~plugins/ga.js', mode: 'client' }
+  ],
   /*
   ** Nuxt.js dev-modules
   */
