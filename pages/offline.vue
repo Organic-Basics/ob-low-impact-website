5114031d8e99c9c85901ffaa524be9d1ad090646
Petya Kazakova <pk.kazakova@gmail.com>
2020-06-10 09:49:46 +0200

    Finished Offline page

--- pages/offline.vue
+++ pages/offline.vue
@@ -6,10 +6,13 @@
 	</header>
   <div class="offline__main">
   	<ImpactMeter/>
+		<h1 class="offline__text--dramatic">Offline</h1>
   	<h5 class="offline__carbon">Carbon intensity: {{carbonIntensity.forecast}}g CO2/kWh</h5>
-  	<p>The website is currently unavailable as the server it is hosted on is running on too little renewable energy.</p>
-  	<br>
-  	<p>It looks like the carbon intensity will get better in <strong>{{minutesTillNext}} minutes</strong>.</p>
+		<div class="offline__desc">
+			<p>The website is currently unavailable as the server it is hosted on is running on too little renewable energy.</p>
+	  	<br>
+	  	<p>It looks like the carbon intensity will get better in <strong>{{minutesTillNext}} minutes</strong>.</p>
+		</div>
   </div>
 </div>
 </template>
@@ -63,6 +66,7 @@ export default Vue.extend({
 
 <style lang="scss">
 @import "~assets/scss/variables.scss";
+@import "~assets/scss/mixins.scss";
 
 .page--offline {
 	align-items: center;
@@ -71,9 +75,13 @@ export default Vue.extend({
 
 	header {
 		max-width: 90vw;
+		display: flex;
+    flex-direction: column;
+    align-items: center;
 
 		.logo {
-			margin-top: 10px;
+			top: 20px;
+			position: absolute;
 		}
 
 		.offline-header__title {
@@ -83,13 +91,57 @@ export default Vue.extend({
 	}
 
 	.offline__main {
+		align-items: center;
+    justify-content: center;
+    flex-direction: column;
+		display: flex;
+
 		svg, p {
 			padding: 0 map-get($mobile, "sidePadding");
+		}
+
+		.offline__text--dramatic {
+			-webkit-text-fill-color: map-get($colors, 'bgGrey');
+			-webkit-text-stroke: 1px map-get($colors, 'carbonHigh');
+	    margin: auto;
+	    position: absolute;
+			margin-top: -70px;
+    	font-size: 15vw;
+
+			@include screenSizes(tabletPortrait) {
+        margin-top: -60px;
+      }
+
+      @include screenSizes(phone) {
+        margin-top: -70px;
+        font-size: 25vw;
+        max-width: 90vw;
+      }
 		}
 
 		.offline__carbon {
 			color: map-get($colors, 'carbonHigh');
 			margin-bottom: 35px;
+		}
+
+		.spedometer__container {
+			height: auto;
+
+			span {
+				display: none;
+			}
+		}
+
+		.offline__desc {
+			max-width: 45vw;
+
+			@include screenSizes(tabletPortrait) {
+				max-width: 80vw;
+			}
+
+			@include screenSizes(phone) {
+				max-width: 100%;
+			}
 		}
 	}
 }
