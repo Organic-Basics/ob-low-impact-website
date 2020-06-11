8244336bb19c802fc9e1aec4b42b0912ea295ff9
Henderson Pinto <hendersoncode@gmail.com>
2020-06-11 13:03:03 +0200

    Added v-html directive to parse the contentful data in case of HTML elements included

--- components/Product/ProductSelect.vue
+++ components/Product/ProductSelect.vue
@@ -4,7 +4,7 @@
       <div>
           <h4 class="product__title">{{mainProduct.title}}</h4>
           <!-- Disabled for now until we can merge the fix -->
-          <span class="product__one-liner product__description--desktop">{{contentfulData.oneLiner}}</span>
+          <span v-html="contentfulData.oneLiner" class="product__one-liner product__description--desktop"></span>
       </div>
       <h3 class="product__main--price">{{parseInt(mainProduct.priceRange.minVariantPrice.amount)}} {{mainProduct.priceRange.minVariantPrice.currencyCode}}
         <span class="product__main--compare-price" v-if="mainProduct.variants.edges[0].node.compareAtPrice !== null && mainProduct.variants.edges[0].node.compareAtPrice !== '0.00'">
@@ -15,7 +15,7 @@
     <div class="product__main--form">
       <!-- Disabled for now until we can merge the fix -->
       <div v-if="isSingleProd">
-        <h6 class="product__one-liner product__description--mobile">{{ contentfulData.oneLiner }}</h6>
+        <h6 v-html="contentfulData.oneLiner" class="product__one-liner product__description--mobile"></h6>
       </div>
       <!-- Single product selection -->
       <div v-if="isSingleProd" class="product__main--selection-container">
